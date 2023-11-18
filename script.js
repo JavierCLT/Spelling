d0abc5e4a95ae041a27d3a9e4b20bf7eebe5f0d7
JavierCLT <132976745+JavierCLT@users.noreply.github.com>
2023-11-17 23:49:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,248 +1,248 @@
-@keyframes fadeIn {
-  from { opacity: 0; }
-  to { opacity: 1; }
+// The list of words for the child to practice
+const originalWordsToPractice = ["apple", "banana", "bathroom", "bedroom", "car", "carrot", "cat", "chair", "chimpanzee", "computer", "dad", "day", "dog", "drums", "ear", "fall", "feet", "fridge", "garlic", "gorilla", "guitar", "hand", "happiness", "harmonica", "hawk", "house", "ice", "jellyfish", "lemon", "lion", "milk", "mom", "motorcycle", "night", "onion", "orange", "parrot", "pepper", "plane", "potato", "salt", "school", "spacecraft", "spring", "summer", "table", "tomato", "violin", "wall", "water", "whale", "winter"];
+
+// Clone the original array to manipulate
+let wordsToPractice = [...originalWordsToPractice];
+
+// Initialize the counter
+let wordsTypedCount = 0;
+
+// Prevent the glitch of re-typing words quickly
+let inputLocked = false;
+
+// Function to play the word sound
+function playWordSound(word, callback) {
+  const wordSound = new Audio(`sounds/word_sounds/English/${word}.mp3`);
+  wordSound.play();
+
+  // When the sound has finished playing, call the callback if provided
+  wordSound.onended = () => {
+    if (callback) {
+      callback();
+    }
+  };
 }
 
-.fade-in {
-  animation: fadeIn 0.5s ease-in forwards;
+// Function to play the letter sound
+function playLetterSound(letter) {
+  const letterSound = new Audio(`sounds/letter%20sounds/${letter.toUpperCase()}.wav`);
+  letterSound.play();
 }
 
-/* Base styles */
-html {
-  font-size: 100%; /* Default to 16px */
+// Function to play the success sound
+function playSuccessSound() {
+  const successSound = new Audio('sounds/success/fanfare.mp3');
+  successSound.play();
 }
 
-body {
-  margin: 0;
-  padding: 0;
-}
-
-.correct-letter {
-  color: green;
-}
-
-.incorrect-letter {
-  color: red;
-}
-
-/* Styles for the image display on desktop */
-#wordImage {
-  display: block;
-  width: 15%; /* Default for desktop */
-  height: auto;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 15px;
-}
-
-/* Adjustments for tablets */
-@media (max-width: 768px) {
-  #wordImage {
-    width: 40%; /* Slightly larger on tablet screens */
-    margin: 15px auto; /* Adjust margin as needed */
-  }
-}
-
-/* Further adjustments for mobile devices */
-@media (max-width: 480px) {
-  #wordImage {
-    width: 60%; /* Even larger on mobile screens */
-    margin: 10px auto; /* Less margin on small screens */
-  }
-}
-
-/* Styles for the word display */
-#wordDisplay {
-  font-family: 'Helvetica', sans-serif;
-  font-size: 7vw; /* Increase the font size */
-  letter-spacing: normal;
-  text-align: center;
-  margin: 0 auto; /* Ensure no side margins are pushing the text off-center */
-  padding: 0; /* Ensure no padding is affecting the centering */
-  margin-bottom: 10px; /* Include padding and borders in the element's total width and height */
-  width: 100%; /* Full width to ensure proper centering */
-}
-
-.word-input::placeholder {
-  font-size: 4vw; /* Adjust the font size of the placeholder text as needed */
-  color: #888; /* Change the color if you want */
-  /* You can add other styles here if needed */
-}
-
-/* Styles for the input field */
-.word-input {
-  font-family: 'Helvetica', sans-serif;
-  font-size: 7vw; /* Increase the font size */
-  width: 90%; /* Slightly less than full width to add some margin */
-  max-width: 800px; /* Ensures it's not too wide on larger screens */
-  padding: 15px;
-  border: 2px solid #ddd;
-  border-radius: 5px;
-  color: transparent;
-  background-color: transparent;
-  text-align: center;
-  position: relative; /* Needed to establish a stacking context for z-index */
-  z-index: 1; /* Ensure the input is behind the overlay */
-  height: 120px;
-  margin-top: 10px;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.input-overlay-container {
-  position: relative; /* Establishes the positioning context for its absolutely positioned children */
-  width: 90%; /* Same width as your input to match sizes */
-  max-width: 800px; /* Same max width as your input */
-  margin: 10px auto; /* Center the container */
-  align-items: center; /* Centers the content vertically */
-  text-align: center;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.styled-overlay {
-  position: absolute; /* Position it directly over the input */
-  top: 0; /* Align to the top of the parent */
-  left: 0; /* Align to the left of the parent */
-  right: 0; /* Stretch to the right edge of the parent */
-  bottom: 0; /* Stretch to the bottom edge of the parent */
-  display: flex; /* Use flexbox to align children */
-  justify-content: center;
-  align-items: center; /* Centers the content vertically */
-  font-family: 'Helvetica', sans-serif;
-  text-align: center;
-  color: transparent; /* Makes the text color transparent */
-  pointer-events: none;
-  margin: 10px auto;
-  font-size: 7vw; /* Same font size as the input to match sizes */
-  z-index: 2; /* Ensures it is on top of the input field */
-  margin-left: auto;
-  margin-right: auto;
-}
-
-/* Styles for the message display */
-.message {
-  font-family: 'Helvetica', sans-serif;
-  font-size: 2vw; /* Increase the font size */
-  text-align: center;
-  color: #333;
-}
-
-/* Center the container */
-.container {
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* This will center children horizontally */
-  width: 100%;
-  margin: 0 auto;
- }
-
-.counter {
-  font-family: 'Helvetica', sans-serif;
-  font-size: 3em;
-  color: #333;
-  text-align: center;
-  margin-top: 20px;
-}
-
-.uppercase-letter {
-  font-size: 0.8em; /* or any size that is suitable */
-}
-
-/* Responsive adjustments with media queries */
-@media (max-width: 768px) {
-  /* Adjustments for tablets and mobile devices */
-  #wordDisplay {
-    font-size: 10vw; /* Further increase for smaller screens */
-    margin-left: auto;
-    margin-right: auto;
-  }
+// This function should visually update the word display with underscores for each letter
+function updateDisplayedWord(word) {
+  const wordDisplay = document.getElementById('wordDisplay');
+  const styledOverlay = document.getElementById('styledOverlay');
+  const wordInput = document.getElementById('wordInput');
   
-  .word-input {
-    font-size: 10vw; /* Adjust the font size for tablets */
-    height: 40px; /* Adjust height for tablets */
-    margin-top: 10px;
-  }
-  
-  .message {
-    font-size: 6vw;
-  }
+  wordDisplay.innerHTML = ''; // Clear the previous word display
+  styledOverlay.innerHTML = ''; // Clear the styled overlay
+  wordInput.value = ''; // Clear the input box
 
-  .container {
-    margin-top: 20px; /* Add some top margin on smaller screens */
+  // Create an underscore for each letter in the word with a margin for separation
+  for (let i = 0; i < word.length; i++) {
+    const underscoreSpan = document.createElement('span');
+    underscoreSpan.textContent = '_';
+    underscoreSpan.style.marginRight = '5px'; // Adjust the margin as needed for separation
+    underscoreSpan.className = 'underscore';
+    underscoreSpan.id = `letter${i}`; // Set the ID for each span to be used later
+    wordDisplay.appendChild(underscoreSpan);
   }
-  
-  .word-input::placeholder {
-  font-size: 5vw; /* Adjust the font size of the placeholder text as needed */
-  color: #888; /* Change the color if you want */
-  /* You can add other styles here if needed */
-}
-.styled-overlay {
-  position: absolute; /* Position it directly over the input */
-  top: 0; /* Align to the top of the parent */
-  left: 0; /* Align to the left of the parent */
-  right: 0; /* Stretch to the right edge of the parent */
-  bottom: 0; /* Stretch to the bottom edge of the parent */
-  display: flex; /* Use flexbox to align children */
-  justify-content: center;
-  align-items: center; /* Centers the content vertically */
-  font-family: 'Helvetica', sans-serif;
-  text-align: center;
-  color: transparent; /* Makes the text color transparent */
-  pointer-events: none;
-  margin: 10px auto;
-  font-size: 10vw; /* Same font size as the input to match sizes */
-  z-index: 2; /* Ensures it is on top of the input field */
-  height: 70px;
-  margin-left: auto;
-  margin-right: auto;
-}  
 }
 
-@media (max-width: 480px) {
-  /* Further adjustments for mobile devices */
-  #wordDisplay {
-    font-size: 10vw; /* Further increase for smaller screens */
-    margin-left: auto;
-    margin-right: auto;
-  }
-  
-  .word-input {
-    font-size: 10vw; /* Adjust the font size for tablets */
-    height: 40px; /* Adjust height for tablets */
-    margin-top: 10px;
-  }
-
-  .message {
-    font-size: 6vw;
-  }
-
-  .container {
-    margin-top: 20px; /* Add some top margin on very small screens */
-  }
-  .word-input::placeholder {
-  font-size: 5vw; /* Adjust the font size of the placeholder text as needed */
-  color: #888; /* Change the color if you want */
-  /* You can add other styles here if needed */
+// Function to show a message below the word input
+function showMessage(message) {
+  const messageElement = document.getElementById('message');
+  messageElement.textContent = message;
 }
-.styled-overlay {
-  position: absolute; /* Position it directly over the input */
-  top: 0; /* Align to the top of the parent */
-  left: 0; /* Align to the left of the parent */
-  right: 0; /* Stretch to the right edge of the parent */
-  bottom: 0; /* Stretch to the bottom edge of the parent */
-  display: flex; /* Use flexbox to align children */
-  justify-content: center;
-  align-items: center; /* Centers the content vertically */
-  font-family: 'Helvetica', sans-serif;
-  text-align: center;
-  color: transparent; /* Makes the text color transparent */
-  pointer-events: none;
-  margin: 10px auto;
-  font-size: 10vw; /* Same font size as the input to match sizes */
-  z-index: 2; /* Ensures it is on top of the input field */
-  height: 70px;
-  margin-left: auto;
-  margin-right: auto;
-}  
+
+// Function to update counter
+function updateWordsTypedCountDisplay() {
+  const countDisplay = document.getElementById('counter');
+  countDisplay.textContent = `${wordsTypedCount}`;
+}
+
+// This function handles the user's input and updates the visual feedback
+function handleKeyPress(event) {
+  // Early exit if input is locked
+  if (inputLocked) {
+    return;
+  }
+  const typedWord = wordInput.value;
+  const currentWord = wordInput.dataset.currentWord;
+
+  updateDisplayedLetters(typedWord, currentWord.toLowerCase());
+  overlayTypedWord(typedWord, currentWord.toLowerCase());
+
+  // If the word is fully and correctly typed (case-insensitive comparison)
+  if (typedWord.toLowerCase() === currentWord.toLowerCase() && typedWord.length === currentWord.length) {
+    handleCorrectWord(currentWord);
+  }
+}
+
+// Function to overlay the styled text
+function overlayTypedWord(typedWord, currentWord) {
+  const overlayElement = document.getElementById('styledOverlay');
+  overlayElement.innerHTML = ''; // Clear the existing content
+
+  for (let i = 0; i < typedWord.length; i++) {
+    const span = document.createElement('span');
+    if (typedWord[i].toLowerCase() === currentWord.toLowerCase()[i]) {
+      span.textContent = typedWord[i];
+      span.classList.add('correct-letter');
+    } else {
+      span.textContent = typedWord[i];
+      span.classList.add('incorrect-letter');
+    }
+
+    // Check if the character is uppercase
+    if (typedWord[i] === typedWord[i].toUpperCase() && /^[A-Z]$/.test(typedWord[i])) {
+      span.classList.add('uppercase-letter');
+    }
+
+    overlayElement.appendChild(span);
+  }
+}
+
+// This new function updates the displayed underscores/letters as the user types
+function updateDisplayedLetters(typedWord, currentWord) {
+  for (let i = 0; i < currentWord.length; i++) {
+    const letterElement = document.getElementById(`letter${i}`);
+
+    if (i < typedWord.length) {
+      // Convert both characters to lowercase for case-insensitive comparison
+      if (typedWord[i].toLowerCase() === currentWord[i].toLowerCase()) {
+        letterElement.textContent = currentWord[i]; // Display as in the original word
+        letterElement.style.marginRight = '0';
+      } else {
+        letterElement.textContent = '_';
+        letterElement.style.marginRight = '5px';
+      }
+    } else {
+      letterElement.textContent = '_';
+      letterElement.style.marginRight = '5px';
+    }
+  }
+}
+
+// This new function handles everything that should happen when the word is typed correctly
+function handleCorrectWord(currentWord) {
+  inputLocked = true;
+
+  // Delay after the last letter sound before playing the word sound again
+  setTimeout(() => {
+    playWordSound(currentWord, () => {
+      // Determine the delay for the success sound based on the word's length
+      let successSoundDelay = determineSuccessSoundDelay(currentWord);
+
+      // Delay the success sound based on the length of the word
+      setTimeout(() => {
+        playSuccessSound();
+      }, successSoundDelay);
+
+      // Show the success message shortly after the success sound starts
+      setTimeout(() => {
+        showMessage('Good job! That\'s correct!');
+        confetti(); // Play the success animation here
+        wordsTypedCount++; // Increment the words typed count
+        updateWordsTypedCountDisplay(); // Update the display
+      }, successSoundDelay - 300);
+
+      // Clear the input and set a new word a bit after the message is displayed
+      setTimeout(() => {
+        wordInput.value = ''; // Clear the input field
+        setNewWord(); // Set a new word
+      }, successSoundDelay + 2000); // This waits a bit after the message to reset
+    });
+  }, 500); // Delay before replaying the word sound after the last letter sound
+}
+
+// Helper function to determine the delay for the success sound based on word length
+function determineSuccessSoundDelay(word) {
+  if (word.length <= 4) {
+    return 400;
+  } else if (word.length >= 5 && word.length <= 9) {
+    return 480;
+  } else { // for 10 letters or more
+    return 600;
+  }
+}
+
+function setNewWord() {
+  // Check if there are no more words to practice
+  if (wordsToPractice.length === 0) {
+    // Reset the wordsToPractice array to start over
+    wordsToPractice = [...originalWordsToPractice];
+    showMessage('All words completed! Starting again...');
+    wordsTypedCount = 0; // Reset the count if needed
+    updateWordsTypedCountDisplay();
+  }
+
+  // Choose a random word from the array
+  const randomIndex = Math.floor(Math.random() * wordsToPractice.length);
+  const newWord = wordsToPractice[randomIndex];
+
+  // Display underscores for the new word
+  updateDisplayedWord(newWord);
+
+  // Set the image source based on the new word
+  const wordImage = document.getElementById('wordImage');
+  wordImage.src = `images/${newWord}.png`;
+  wordImage.style.display = 'block'; // Show the image
+
+  // Remove the used word from the array
+  wordsToPractice.splice(randomIndex, 1);
+
+  // Store the current word in the dataset and set the maxlength attribute
+  wordInput.dataset.currentWord = newWord;
+  wordInput.setAttribute('maxlength', newWord.length);
+
+  // Clear any previous messages
+  showMessage('');
+
+  // Unlock the input for the new word
+  inputLocked = false;
+
+  // Clear the input field and the styled overlay for the new word
+  wordInput.value = '';
+  document.getElementById('styledOverlay').innerHTML = '';
+
+  // Play the sound of the new word
+  playWordSound(newWord);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.querySelector('.container');
+  container.classList.add('fade-in');
+
+  // Initialize wordInput here after the DOM has loaded
+  const wordInput = document.getElementById('wordInput');
+  wordInput.placeholder = "Press Enter to listen";
+
+  // Combined keyup event listener for playing letter sounds and the entire word
+  wordInput.addEventListener('keyup', function(event) {
+    // Play the sound of the whole word when Enter is pressed
+    if (event.code === 'Enter') {
+      event.preventDefault(); // Prevent any default action
+      playWordSound(wordInput.dataset.currentWord);
+    }
+    // Play the sound of the letter typed
+    else if (event.key.length === 1 && event.key.match(/[a-z]/i)) {
+      playLetterSound(event.key);
+    }
+  });
+
+  // Set the initial word and focus on the input field
+  setNewWord();
+  wordInput.focus();
+  
+  // Now it's safe to add event listeners to wordInput
+  wordInput.addEventListener('input', handleKeyPress);
+});  
 
